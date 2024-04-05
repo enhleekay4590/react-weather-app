ba70afee4792872997de1ae009fe7a080ffb9e65
EnhleeKay <minenhle.kubheka241@gmail.com>
2024-04-05 23:36:32 +0200

    Added the GitHub url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App </h1>
+      <footer>
+        This project was coded by
+        <a href="#" target="_blank">
+          {" "}
+          Minenhle S. Kubheka
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/enhleekay4590/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
